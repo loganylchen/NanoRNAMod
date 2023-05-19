24944c54768daf6e93cec984df3fec6a14b60781
logan <yuelong.chen.btr@gmail.com>
2023-05-19 16:30:43 +0800

    add two logs

--- workflow/scripts/baleen_dataprep.py
+++ workflow/scripts/baleen_dataprep.py
@@ -5,7 +5,7 @@ from baleen.workflows import data_prep
 
 
 sys.stderr = open(snakemake.log[0], "w")
-
+sys.stdout = open(snakemake.log[0], "a")
 
 data_prep(snakemake.input.eventalign,snakemake.output[0], snakemake.params.label,threads=snakemake.threads,verbose=False)
 
