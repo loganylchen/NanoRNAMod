116f2144a56a2c32b70a808c1a0a0358775b8970
logan <yuelong.chen.btr@gmail.com>
2023-05-19 16:31:21 +0800

    tmp

--- workflow/scripts/baleen_dataprep.py
+++ workflow/scripts/baleen_dataprep.py
@@ -7,6 +7,8 @@ from baleen.workflows import data_prep
 sys.stderr = open(snakemake.log[0], "w")
 sys.stdout = open(snakemake.log[0], "a")
 
+
+print(snakemake.input.eventalign,snakemake.output[0], snakemake.params.label,snakemake.threads)
 data_prep(snakemake.input.eventalign,snakemake.output[0], snakemake.params.label,threads=snakemake.threads,verbose=False)
 
 
