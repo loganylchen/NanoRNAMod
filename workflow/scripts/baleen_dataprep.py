88020241006d18d292cd75d0d82c35061ac23f7c
logan <yuelong.chen.btr@gmail.com>
2023-05-19 16:38:09 +0800

    makedir

--- workflow/scripts/baleen_dataprep.py
+++ workflow/scripts/baleen_dataprep.py
@@ -1,6 +1,7 @@
 
 import yaml
 import sys
+import os
 from baleen.workflows import data_prep
 
 
@@ -8,6 +9,8 @@ sys.stderr = open(snakemake.log[1], "w")
 sys.stdout = open(snakemake.log[0], "w")
 
 
+
+os.makedirs(snakemake.output[0],exist_ok=True)
 print(snakemake.input.eventalign,snakemake.output[0], snakemake.params.label,snakemake.threads)
 data_prep(snakemake.input.eventalign,snakemake.output[0], snakemake.params.label,threads=snakemake.threads,verbose=False)
 
