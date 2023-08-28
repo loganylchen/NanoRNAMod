b58822306c5cf85d7a7201dad7716715d8cc93c0
Yuelong Chen <yuelong.chen.btr@gmail.com>
2023-08-28 14:46:52 +0800

    mapping list

--- workflow/scripts/mapping_list.py
+++ workflow/scripts/mapping_list.py
@@ -16,8 +16,8 @@ with open(snakemake.input.mapping_list, 'r') as mapping_list, open(snakemake.out
         transcript, fastq,fasta,bam = line.strip().split('\t')
         print(f'Working on {transcript}')
         bam_list.write(f'{bam}\n')
-        subprocess.check_call(f"minimap2 -t {snakemake.threads} {snakemake.params.extra} {fasta} {fastq} | samtools view -Sbh | samtools sort - -o {bam}",shell=True)
-        subprocess.check_call(f'samtools index {bam}',shell=True)
+        subprocess.check_call(f"minimap2 -t {snakemake.threads} {snakemake.params.extra} {fasta} {fastq} | samtools view -Sbh | samtools sort - -o {bam}",shell=True,stderr=sys.stdout,stdout=sys.stdout)
+        subprocess.check_call(f'samtools index {bam}',shell=True,stderr=sys.stdout,stdout=sys.stdout)
 
 with open(f"{snakemake.params.transcriptome_fasta}.fai", 'r') as fai, open(f"{snakemake.input.mapping_dir}/header.tmp", 'w') as header:
     for line in fai:
@@ -25,9 +25,9 @@ with open(f"{snakemake.params.transcriptome_fasta}.fai", 'r') as fai, open(f"{sn
         header.write(f'@SQ\tSN:{transcript}\tLN:{length}\n')
 
 print('Merging')
-subprocess.check_call(f"samtools merge -b {snakemake.output.bam_list} -h {snakemake.input.mapping_dir}/header.tmp | samtools sort - -o {snakemake.output.bam} ",shell=True)
-subprocess.check_call(f"samtools index {snakemake.output.bam}",shell=True)
-subprocess.check_call(f"rm {snakemake.input.mapping_dir}/header.tmp",shell=True)
+subprocess.check_call(f"samtools merge -b {snakemake.output.bam_list} -h {snakemake.input.mapping_dir}/header.tmp | samtools sort - -o {snakemake.output.bam} ",shell=True,stderr=sys.stdout,stdout=sys.stdout)
+subprocess.check_call(f"samtools index {snakemake.output.bam}",shell=True,stderr=sys.stdout,stdout=sys.stdout)
+subprocess.check_call(f"rm {snakemake.input.mapping_dir}/header.tmp",shell=True,stderr=sys.stdout,stdout=sys.stdout)
 
 
 
