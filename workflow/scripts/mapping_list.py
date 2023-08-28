2bc801cb4e4b884e619065e3ae8b1a504d3a53ab
Yuelong Chen <yuelong.chen.btr@gmail.com>
2023-08-28 14:40:41 +0800

    mapping list

--- workflow/scripts/mapping_list.py
+++ workflow/scripts/mapping_list.py
@@ -11,7 +11,7 @@ sys.stderr = sys.stdout
 
 
 
-with (open(snakemake.input.mapping_list, 'r') as mapping_list, open(snakemake.output.bam_list,'w') as bam_list):
+with open(snakemake.input.mapping_list, 'r') as mapping_list, open(snakemake.output.bam_list,'w') as bam_list:
     for line in mapping_list:
         transcript, fastq,fasta,bam = line.strip().split('\t')
         print(f'Working on {transcript}')
@@ -19,7 +19,7 @@ with (open(snakemake.input.mapping_list, 'r') as mapping_list, open(snakemake.ou
         shell(f"minimap2 -t {snakemake.threads} {snakemake.params.extra} {fasta} {fastq} | samtools view -Sbh | samtools sort - -o {bam}")
         shell(f'samtools index {bam}')
 
-with (open(f"{snakemake.params.transcriptome_fasta}.fai", 'r') as fai, open(f"{snakemake.input.mapping_dir}/header.tmp", 'w') as header):
+with open(f"{snakemake.params.transcriptome_fasta}.fai", 'r') as fai, open(f"{snakemake.input.mapping_dir}/header.tmp", 'w') as header:
     for line in fai:
         transcript, length, *_ = line.strip().split('\t')
         header.write(f'@SQ\tSN:{transcript}\tLN:{length}\n')
