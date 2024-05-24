c8532e89347234f11d3ee11fd7427c0c3eedc1e2
Yuelong Chen <yuelong.chen.btr@gmail.com>
2024-05-24 11:18:57 +0800

    epinano

--- workflow/scripts/epinano_prep.sh
+++ workflow/scripts/epinano_prep.sh
@@ -6,7 +6,7 @@ set -x
 set -e
 
 python /opt/Epinano/Epinano_Variants.py  -s /opt/Epinano/misc/sam2tsv.jar  -R "${snakemake_input[reference]}" \
--b "${snakemake_input[sample_bam]}" -n {snakemake[threads]} -T t 2>"${snakemake_log[0]}"
+-b "${snakemake_input[sample_bam]}" -n ${snakemake[threads]} -T t 2>"${snakemake_log[0]}"
 
 python /opt/Epinano/misc/Epinano_sumErr.py --kmer 0 --file "${snakemake_output[per_site]}" \
     --out "${snakemake_output[sum_err]}" 2>> "${snakemake_log[0]}"
