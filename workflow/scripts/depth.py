fa4d7bd67f4e1c449bd188451ee28860154f5b39
logan <yuelong.chen.btr@gmail.com>
2024-06-18 23:02:11 +0800

    add depth

--- workflow/scripts/depth.py
+++ workflow/scripts/depth.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 df = pd.read_csv(snakemake.input[0],sep="\t",header=None)
 df.columns = ["chrom","pos"] + snakemake.params.samples
-df.to_csv(snakemake.output[0],sep="\t")
+df.to_csv(snakemake.output[0],sep="\t",index=False)
