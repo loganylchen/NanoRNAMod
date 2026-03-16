656647297d86342510aaee8e8be4c70306896ee5
loganylchen <yuelongchen@cuhk.edu.hk>
2026-03-16 14:21:28 +0800

    Comment out unused variables and scripts in epinano_prep for clarity

--- workflow/scripts/epinano_prep.sh
+++ workflow/scripts/epinano_prep.sh
@@ -5,11 +5,11 @@
 set -x
 set -e
 
-python /opt/epinano/Epinano_Variants.py  -s /opt/epinano/misc/sam2tsv.jar  -R "${snakemake_input[reference]}" \
--b "${snakemake_input[sample_bam]}" -n ${snakemake[threads]} -T t 2>"${snakemake_log[0]}"
+python /opt/epinano/Epinano_Variants.py   -r "${snakemake_input[reference]}" \
+-b "${snakemake_input[sample_bam]}" -c ${snakemake[threads]}  2>"${snakemake_log[0]}"
 
-python /opt/epinano/misc/Epinano_sumErr.py --kmer 0 --file "${snakemake_output[per_site]}" \
-    --out "${snakemake_output[sum_err]}" 2>> "${snakemake_log[0]}"
+# python /opt/epinano/misc/Epinano_sumErr.py --kmer 0 --file "${snakemake_output[per_site]}" \
+#     --out "${snakemake_output[sum_err]}" 2>> "${snakemake_log[0]}"
 
 #python /opt/epinano/misc/Slide_Variants.py "${snakemake_output[per_site]}" 5
 #python /opt/epinano/Epinano_Predict.py  \
