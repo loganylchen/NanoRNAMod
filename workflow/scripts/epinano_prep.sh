c6df268e753a95912fbc8647b7bc04a573121589
loganylchen <yuelongchen@cuhk.edu.hk>
2026-03-30 14:38:56 +0800

    Fix epinano_prep: redirect both stdout and stderr to log
    Use exec &> to capture all output from both commands into the log file.

--- workflow/scripts/epinano_prep.sh
+++ workflow/scripts/epinano_prep.sh
@@ -3,13 +3,13 @@
 set -x
 set -e
 
+exec &>"${snakemake_log[0]}"
+
 python /opt/epinano/Epinano_Variants.py -s /opt/epinano/misc/sam2tsv.jar \
     -R "${snakemake_input[reference]}" \
     -b "${snakemake_input[sample_bam]}" \
-    -t ${snakemake[threads]} -T t \
-    2>"${snakemake_log[0]}"
+    -t ${snakemake[threads]} -T t
 
 python /opt/epinano/misc/Epinano_sumErr.py --kmer 0 \
     --file "${snakemake_output[per_site]}" \
-    --out "${snakemake_output[sum_err]}" \
-    2>>"${snakemake_log[0]}"
+    --out "${snakemake_output[sum_err]}"
