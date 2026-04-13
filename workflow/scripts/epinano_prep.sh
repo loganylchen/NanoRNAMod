c6c440a3bb5c0588d584592bc8648f2288ccb88b
Yuelong CHEN <yuelongchen@cuhk.edu.hk>
2026-04-13 11:58:05 +0800

    Fix incorrect -R flag in epinano_prep.sh (should be -r)
    Epinano_Variants.py expects -r/--reference, not -R.

--- workflow/scripts/epinano_prep.sh
+++ workflow/scripts/epinano_prep.sh
@@ -6,7 +6,7 @@ set -e
 exec &>"${snakemake_log[0]}"
 
 python /opt/epinano/Epinano_Variants.py -s /opt/epinano/misc/sam2tsv.jar \
-    -R "${snakemake_input[reference]}" \
+    -r "${snakemake_input[reference]}" \
     -b "${snakemake_input[sample_bam]}" \
     -t ${snakemake[threads]} -T t
 
